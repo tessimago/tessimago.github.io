4feb244289d2fac591ed6da3836d6bb220e4e0a9
aimbotduda <da.gabriel@campus.fct.unl.pt>
2023-10-03 12:22:57 +0100

    small ui improvements

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let menu_icon_box = document.querySelector(".menu_icon_box");
 let box = document.querySelector(".box");
 
 menu_icon_box.onclick = function () {
-    menu_icon_box.classList.toggle("active");
+    menu_icon_box.classList.toggle("active_menu_icon_box");
     box.classList.toggle("active_box");
 };
 
